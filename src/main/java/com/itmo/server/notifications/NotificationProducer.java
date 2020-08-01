af8658195662b81d911923e7335e3534eedd7705
spicyfalafel <58147555+spicyfalafel@users.noreply.github.com>
2020-08-01 04:59:06 +0300

    update NotificationProducer.java from android lol

--- src/main/java/com/itmo/server/notifications/NotificationProducer.java
+++ src/main/java/com/itmo/server/notifications/NotificationProducer.java
@@ -1,4 +1,13 @@
 package com.itmo.server.notifications;
 
 public class NotificationProducer {
+  private List<SocketChannel> channels;
+  private[] data;
+  
+  
+  private void setNotification(Notification notification){
+    channels.forEach(c -> {
+      c.write()
+    });
+  }
 }
